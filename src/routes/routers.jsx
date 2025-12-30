984377f3002e2c6eb070b473b0a20d5a12a49c44
4mahbubur4 <mahbubur.mr4@gmail.com>
2025-12-30 21:56:00 +0600

    Started assainment 9

---
+++ src/routes/routers.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter(
+    [
+        {
+            path:"/",
+            element: <h2>This is home</h2>
+        },
+        {
+            path: "/auth",
+            element: <h2>Authentication Layout</h2>
+        },
+        {
+            path:'/news',
+            element: <h2>News</h2>
+        },
+        {
+            path:'/*',
+            element: <h2>Error -404</h2>
+        },
+    ]
+);
+export default router;
